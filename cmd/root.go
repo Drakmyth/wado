48df28796ad4d3169490b12c43cfa3b4babe2599
Drakmyth <shaun.hamman@gmail.com>
2024-05-21 22:34:03 -0700

    Remove root command handler

--- cmd/root.go
+++ cmd/root.go
@@ -9,9 +9,6 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "wado",
 	Short: "A tool for mixing and munging Doom WAD files.",
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: print help
-	},
 }
 
 func Execute() {
